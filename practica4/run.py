2cc08c1edb50e094810b4ff98f0190ecd6dedb31
Jaime Frías <jaimefrias_76@hotmail.com>
2018-11-12 02:14:36 +0100

    Casi terminada p4

---
+++ practica4/run.py
@@ -0,0 +1,248 @@
+from flask import Flask, request, render_template, session, redirect
+from jinja2 import Template, Environment, PackageLoader, select_autoescape
+import pymongo
+
+# Conecting to Mongo DB
+try:
+    conn = pymongo.MongoClient()
+    print("Connected successfully")
+except pymongo.errors.ConnectionFailure:
+    print ("ASDSAD")
+
+
+db2 = conn.mydb
+
+app = Flask(__name__)
+
+env = Environment(
+	loader=PackageLoader('app', 'templates'),
+	autoescape=select_autoescape(['html', 'xml'])
+)
+
+""" TEMPLATES """
+index_t = env.get_template('index.html')
+error_t   = env.get_template('error.html')
+login_t   = env.get_template('login.html')
+posts_t     = env.get_template('posts.html')
+last_pages     = env.get_template('lastpages.html')
+register_t     = env.get_template('register.html')
+profile_t     = env.get_template('profile.html')
+settings_t = env.get_template('settings.html')
+
+"""Save current page with log name in db"""
+def save_page(log):
+    actual = session['username']
+    if db2.pages.find({"username": actual}).count() > 0:
+        db2.pages.update({"username": actual}, {"$push": {"pages": log}})
+        if len(db2.pages.find_one({"username": actual})['pages']) > 5:
+            db2.pages.update({"username": actual},{"$pop": {"pages": -1}}  )
+    else:
+        array_nuevo = [log]
+        db2.pages.insert({"username": actual, "pages": array_nuevo})
+
+""" Checks if user is logued """
+def login_method(request):
+    if 'username' not in session:
+        if request.method == 'POST':
+            username = request.form.get('username')
+            password = request.form.get('password')
+            if db2.users.find({"username":username, "password": password}):
+                session['username'] = username
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return True
+
+
+
+"""////////////  LOGIN PAGE   //////////////"""
+
+@app.route('/login')
+def login_page():
+    if 'username' in session:
+        return redirect("/", code=302)
+
+    return login_t.render(session=session, login_page=True)
+
+@app.route('/login', methods=['POST'])
+def post_login_page():
+    if 'username' in session:
+        return redirect("/", code=302)
+
+    if not login_method(request):
+        error = "Username and password doesn't match in our database."
+    else:
+        error = ""
+        return redirect("/", code=302)
+
+    return login_t.render(session=session, error = error, login_page = True)
+
+@app.route('/logout', methods=['get', 'post'])
+def logout():
+    session.pop('username', None)
+    return redirect("/", code=302)
+
+
+@app.route('/register')
+def register():
+    if 'username' in session:
+        return redirect("/", code=302)
+
+    return register_t.render(session=session, login_page = True)
+
+
+@app.route('/register', methods=['POST'])
+def post_register():
+    if 'username' in session:
+        return redirect("/", code=302)
+
+    error = ""
+    if request.method == 'POST':
+        username = request.form.get('username')
+        if len(request.form.get('username')) > 3:
+            id = db2.users.insert( {"username": username, "name": request.form.get('name'), "password": request.form.get('password')})
+            session['username'] = username
+            return redirect("/", code=302)
+        else:
+            error = "Username too short!"
+
+    return register_t.render(session=session, error=error)
+
+@app.route('/user/<user>', methods=['GET'])
+def profile(user):
+    if 'username' in session:
+        save_page("/user/" + user)
+
+    print(db2.users.find_one({"username": user}))
+    return profile_t.render(session=session, profile=db2.users.find_one({"username": user})['name'], user=user)
+
+"""////////////  INDEX PAGE   //////////////"""
+
+@app.route('/')
+def index():
+    error = ""
+    if 'username' in session:
+        save_page("/")
+
+    return index_t.render(session=session)
+
+@app.route('/', methods=['post'])
+def post_index():
+    error = ""
+    if 'username' in session:
+        save_page("/index")
+
+    if request.method == "POST":
+        if len(request.form.get("username")) > 3 and len(request.form.get("password")):
+            login_method(request)
+        else:
+            return redirect("/login", code=302)
+
+    return index_t.render(session=session)
+
+"""////////////  MAIN PAGE   //////////////"""
+
+@app.route('/feed', methods=['get','post'])
+def login():
+    if 'username' in session:
+        save_page("/index")
+    else:
+        if not login_method(request):
+            return redirect("/login", code=302)
+        return redirect("/login", code=302)
+    return article.render(session=session)
+
+"""////////////  POSTS PAGE   //////////////"""
+
+@app.route('/posts', methods=['get','post'])
+def posts():
+    if 'username' not in session:
+        return redirect("/login", 302)
+
+    save_page("/posts")
+    posts = []
+    cursor = db2.posts.find()
+
+    for r in cursor:
+        posts.append(r)
+
+    return posts_t.render(session=session, posts = posts)
+    
+
+@app.route('/newpost', methods=['get','post'])
+def newpost():
+    if 'username' not in session:
+        return redirect("/login", 302)
+    if request.method == 'POST':
+        title = request.form.get('title')
+        text = request.form.get('text')
+
+    db2.posts.insert({"title": title, "text": text, "user": session['username']})
+
+    return redirect("/posts", 302)
+
+@app.route('/deleteposts', methods=['get','post'])
+def deleteposts():
+    if 'username' not in session:
+        return redirect("/login", 302)
+    db2.posts.remove({"user": session['username']})
+    return redirect("/posts", 302)
+
+"""////////////  LAST PAGES   //////////////"""
+
+@app.route('/last-pages', methods=['get','post'])
+def lastPages():
+    if 'username' not in session:
+        return redirect("/login", 302)
+
+    save_page('/last-pages')
+
+    return last_pages.render(session = session, pages = db2.pages.find_one({"username": session['username']})['pages'])
+
+
+@app.route('/settings')
+def settings():
+    if 'username' not in session:
+        return redirect("/login", 302)
+
+    return settings_t.render(session = session)
+
+@app.route('/settings', methods=['post'])
+def post_settings():
+    if 'username' not in session:
+        return redirect("/login", 302)
+
+    save_page('/settings')
+
+    print_name = ""
+    print_password = ""
+
+    if request.method == 'POST':
+        name = request.form.get('name')
+        password = request.form.get('password')
+
+        if name != '':
+            """ Save name """
+            db2.users.update({"username": session['username']},{"name": name})
+            print_name = "Display name changed successfully to " + name + "!"
+            
+        if password != '':
+            db2.users.update({"username": session['username']}, {"password": password})
+            print_password = "Password changed successfully!"
+
+
+    return settings_t.render(session = session, print_name = print_name, print_password = print_password)
+    
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return error_t.render()
+
+if __name__ == '__main__':
+    app.secret_key = 'super secret key'
+    app.config['SESSION_TYPE'] = 'filesystem'
+    app.debug = True
+    app.run(host='0.0.0.0', debug=True)
